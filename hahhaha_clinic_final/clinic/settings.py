b8e2b389bb1d6d0fb3f9fe58be1ef77763b9abfd
nglhongphuong <nglhongphuong@gmail.com>
2024-12-26 17:11:49 +0700

    update

--- hahhaha_clinic_final/clinic/settings.py
+++
@@ -1,4 +0,0 @@
-VNPAY_TMN_CODE = "AVUVZ4VG"  # Mã TmnCode của bạn
-VNPAY_HASH_SECRET_KEY = "OJ0SYOB5BWQ6TS0WDZPDWN6SBFHIWTEI"  # Chuỗi bí mật của bạn
-VNPAY_PAYMENT_URL = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html"  # URL môi trường Sandbox
-VNPAY_RETURN_URL = "http://127.0.0.1:5000/payment_return_vnpay"  # URL nhận callback sau thanh toán
